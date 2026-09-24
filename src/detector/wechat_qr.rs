cb380925ee7038297101c2d2d70d1fb99087171b
Reynard Adimas <adimas.nabil@m3.formulatrix.com>
2026-09-24 16:17:51 +0700

    add wechat_qr module

---
+++ src/detector/wechat_qr.rs
@@ -0,0 +1,32 @@
+use super::{BarcodeDetector, DecodedBarcode}; 
+use crate::model::ModelPaths; 
+use opencv::core::Mat; 
+use opencv::wechat_qrcode::WeChatQRCode; 
+
+pub struct WeChatDetector {
+    inner: WeChatDetector
+} 
+
+impl WeChatDetector {
+    pub fn new(paths: &ModelPaths) -> Result<Self, String> {
+        let inner = WeChatQRCode::new(
+            &paths.detect_protoxt, 
+            &paths.detect_caffemodel, 
+            &paths.sr_prototxt, 
+            &paths.sr_caffemodel
+        )
+        .map_err(|e| e.to_string())?;
+        Ok(Self {inner})
+    }
+} 
+
+impl BarcodeDetector for WeChatDetector {
+    fn detect(&mut self, frame: &Mat) -> Result<Vec<DecodedBarcode>, String> {
+        let mut points = opencv::core::Vector::<Mat>::new();
+        let data = self
+            .inner
+            .detect_and_decode(frame, &mut points)
+            .map_err(|e| e.to_string())?;
+        Ok(vec![DecodedBarcode {data, points: vec![]} ])
+    }
+}
